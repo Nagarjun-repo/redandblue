f2a35c6d534e475efdbecb4df7331b3fb0d73bc9
Nagarjun <nagarjun.sraj@gmail.com>
2022-04-25 12:05:35 +0530

    commiting the changes to auth_servlet.java

--- WEB-INF/classes/auth_servlet.java
+++ WEB-INF/classes/auth_servlet.java
@@ -1,23 +1,29 @@
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import databaseConnectorAPI.DatabaseConnectorAPI;
+import databaseconnectorapi.DatabaseConnectorAPI;
 
 public class auth_servlet extends HttpServlet{
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse res){
         String uname = "email";
-        String passkey = "pwd";
+        String pass = "pwd";
+
         String email = req.getParameter(uname);
-        String pwd  = req.getParameter(passkey);
-        
-        int userId = DatabaseConnectorAPI.validateSigninInfo(email,pwd);
+        String pwd = req.getParameter(pass);
 
-        if (userId>0){
+        int userId = DatabaseConnectorAPI.validateSigninInfo(email, pwd);
 
-        }   
+        if(userId > 0){
+            //redirect to userDashboard
+        }
         else{
-            //redirect to index.html
-        } 
+            try{
+                res.sendRedirect("invalidUser.html");
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
     }
 }
---
+++ WEB-INF/classes/createUser_servlet.java
@@ -0,0 +1,43 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import databaseconnectorapi.search_db;
+import databaseconnectorapi.DatabaseConnectorAPI.insertSignupTimeData;
+
+public class createUser_servlet extends HttpServlet{
+    @Override
+    public void doPost(HttpServletRequest req ,HttpServletResponse res){
+        String firName = "fname";
+        String lastName = "lname";
+        String mobile = "mobno";
+        String uname = "uname";
+        String pass = "pwd";
+
+        String fname = req.getParameter(firName);
+        String lname = req.getParameter(lastName);
+        Long mobNo = Long.parseLong(req.getParameter(mobile));
+        String emailId = req.getParameter(uname);
+        String pwd = req.getParameter(pass);
+
+        int existsId = search_db.search_db(emailId);
+
+        if(existsId == 0){
+            try{
+                Runnable thread_insertSignUpData = new insertSignupTimeData(fname, lname,mobNo,emailId, pwd);
+                new Thread(thread_insertSignUpData).start();
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+        
+        else{
+            try{
+                res.sendRedirect("invalidUser.html");
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
